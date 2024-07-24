2616b39435aa93b12d94db7e6693e494eca06c66
Stuart Leeks <stuartle@microsoft.com>
2024-07-24 12:18:35 +0000

    Tweak basic load pattern

--- end_to_end_tests/scenario_batch_processing_alt1.py
+++ end_to_end_tests/scenario_batch_processing_alt1.py
@@ -25,7 +25,18 @@ from common.config import (
 )
 
 test_start_time = None
-deployment_name = "embedding"
+deployment_name = "embedding100k"
+
+#
+# model deployments:
+#
+# embedding
+#  - 10k TPM
+#  - 60 RPM (1 RPS)
+#
+# embedding100k
+#  - 100k TPM
+#  - 600 RPM (10 RPS)
 
 
 class StagesShape(LoadTestShape):
@@ -35,8 +46,13 @@ class StagesShape(LoadTestShape):
 
     # See https://docs.locust.io/en/stable/custom-load-shape.html
     stages = [
-        {"duration": 120, "users": 2, "spawn_rate": 0.1},
-        {"duration": 300, "users": 6, "spawn_rate": 1},
+        # {"duration": 120, "users": 2, "spawn_rate": 0.1},
+        # {"duration": 300, "users": 6, "spawn_rate": 1},
+        {"duration": 60, "users": 4, "spawn_rate": 1},
+        {"duration": 120, "users": 8, "spawn_rate": 1},
+        {"duration": 180, "users": 12, "spawn_rate": 1},
+        {"duration": 240, "users": 16, "spawn_rate": 1},
+        {"duration": 300, "users": 20, "spawn_rate": 1},
     ]
 
     def tick(self):
@@ -52,7 +68,8 @@ class StagesShape(LoadTestShape):
 # use short input text to validate request-based limiting, longer text to validate token-based limiting
 # TODO - split tests!
 # input_text = "This is some text to generate embeddings for
-input_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitant morbi tristique senectus et netus et malesuada. Bibendum neque egestas congue quisque egestas diam. Rutrum quisque non tellus orci ac auctor augue. Diam in arcu cursus euismod quis. Euismod elementum nisi quis eleifend quam adipiscing. Posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu. Adipiscing diam donec adipiscing tristique risus nec. Nec ultrices dui sapien eget mi proin. Odio facilisis mauris sit amet. Eget aliquet nibh praesent tristique magna. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus vel. Maecenas volutpat blandit aliquam etiam erat velit scelerisque in dictum. Venenatis tellus in metus vulputate. Aliquet enim tortor at auctor urna nunc id cursus metus. Sed velit dignissim sodales ut eu sem integer vitae justo. Posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Egestas sed tempus urna et. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt. Quis viverra nibh cras pulvinar mattis nunc sed. Morbi tempus iaculis urna id. Nisl purus in mollis nunc. Suspendisse potenti nullam ac tortor vitae purus faucibus ornare suspendisse. Nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Semper auctor neque vitae tempus. Euismod lacinia at quis risus sed vulputate. Vitae et leo duis ut diam quam nulla porttitor massa. Sed viverra tellus in hac habitasse platea dictumst. Euismod quis viverra nibh cras pulvinar. Vivamus arcu felis bibendum ut tristique. Lectus proin nibh nisl condimentum id venenatis a. Quis hendrerit dolor magna eget. Neque ornare aenean euismod elementum nisi quis eleifend. Ipsum a arcu cursus vitae congue mauris. Consequat mauris nunc congue nisi vitae suscipit. Libero volutpat sed cras ornare. Malesuada proin libero nunc consequat interdum varius sit amet. Dis parturient montes nascetur ridiculus mus mauris. Penatibus et magnis dis parturient. Nibh ipsum consequat nisl vel. Dictum at tempor commodo ullamcorper a lacus vestibulum. Euismod nisi porta lorem mollis aliquam ut. Dignissim diam quis enim lobortis scelerisque fermentum dui faucibus. Pellentesque sit amet porttitor eget dolor morbi. Pharetra sit amet aliquam id diam maecenas. Volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim. Sodales ut etiam sit amet nisl purus in mollis. Semper risus in hendrerit gravida rutrum. Natoque penatibus et magnis dis parturient. Ornare quam viverra orci sagittis eu volutpat odio. Tristique sollicitudin nibh sit amet commodo nulla facilisi. Laoreet suspendisse interdum consectetur libero id. Lectus urna duis convallis convallis. Bibendum ut tristique et egestas quis ipsum suspendisse. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget. Placerat vestibulum lectus mauris ultrices eros in cursus. Sed turpis tincidunt id aliquet. Tristique senectus et netus et malesuada fames. Ut placerat orci nulla pellentesque dignissim enim sit amet venenatis. Vitae proin sagittis nisl rhoncus mattis. Diam donec adipiscing tristique risus nec. Venenatis tellus in metus vulputate eu scelerisque felis. Dis parturient montes nascetur ridiculus mus mauris vitae. Scelerisque purus semper eget duis at tellus. Vel elit scelerisque mauris pellentesque pulvinar pellentesque. Dictum sit amet justo donec. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Tristique et egestas quis ipsum suspendisse ultrices gravida dictum fusce. Facilisi cras fermentum odio eu feugiat pretium nibh ipsum. Congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque. Sed felis eget velit aliquet sagittis id. Fermentum leo vel orci porta. Lectus vestibulum mattis ullamcorper velit sed ullamcorper. Vitae proin sagittis nisl rhoncus. Habitant morbi tristique senectus et netus. Nisl vel pretium lectus quam id leo in vitae turpis."
+input_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitant morbi tristique senectus et netus et malesuada. Bibendum neque egestas congue quisque egestas diam. Rutrum quisque non tellus orci ac auctor augue. Diam in arcu cursus euismod quis. Euismod elementum nisi quis eleifend quam adipiscing. Posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu. Adipiscing diam donec adipiscing tristique risus nec. Nec ultrices dui sapien eget mi proin. Odio facilisis mauris sit amet. Eget aliquet nibh praesent tristique magna. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus vel. Maecenas volutpat blandit aliquam etiam erat velit scelerisque in dictum. Venenatis tellus in metus vulputate. Aliquet enim tortor at auctor urna nunc id cursus metus. Sed velit dignissim sodales ut eu sem integer vitae justo."
+# input_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitant morbi tristique senectus et netus et malesuada. Bibendum neque egestas congue quisque egestas diam. Rutrum quisque non tellus orci ac auctor augue. Diam in arcu cursus euismod quis. Euismod elementum nisi quis eleifend quam adipiscing. Posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu. Adipiscing diam donec adipiscing tristique risus nec. Nec ultrices dui sapien eget mi proin. Odio facilisis mauris sit amet. Eget aliquet nibh praesent tristique magna. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus vel. Maecenas volutpat blandit aliquam etiam erat velit scelerisque in dictum. Venenatis tellus in metus vulputate. Aliquet enim tortor at auctor urna nunc id cursus metus. Sed velit dignissim sodales ut eu sem integer vitae justo. Posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Egestas sed tempus urna et. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt. Quis viverra nibh cras pulvinar mattis nunc sed. Morbi tempus iaculis urna id. Nisl purus in mollis nunc. Suspendisse potenti nullam ac tortor vitae purus faucibus ornare suspendisse. Nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Semper auctor neque vitae tempus. Euismod lacinia at quis risus sed vulputate. Vitae et leo duis ut diam quam nulla porttitor massa. Sed viverra tellus in hac habitasse platea dictumst. Euismod quis viverra nibh cras pulvinar. Vivamus arcu felis bibendum ut tristique. Lectus proin nibh nisl condimentum id venenatis a. Quis hendrerit dolor magna eget. Neque ornare aenean euismod elementum nisi quis eleifend. Ipsum a arcu cursus vitae congue mauris. Consequat mauris nunc congue nisi vitae suscipit. Libero volutpat sed cras ornare. Malesuada proin libero nunc consequat interdum varius sit amet. Dis parturient montes nascetur ridiculus mus mauris. Penatibus et magnis dis parturient. Nibh ipsum consequat nisl vel. Dictum at tempor commodo ullamcorper a lacus vestibulum. Euismod nisi porta lorem mollis aliquam ut. Dignissim diam quis enim lobortis scelerisque fermentum dui faucibus. Pellentesque sit amet porttitor eget dolor morbi. Pharetra sit amet aliquam id diam maecenas. Volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim. Sodales ut etiam sit amet nisl purus in mollis. Semper risus in hendrerit gravida rutrum. Natoque penatibus et magnis dis parturient. Ornare quam viverra orci sagittis eu volutpat odio. Tristique sollicitudin nibh sit amet commodo nulla facilisi. Laoreet suspendisse interdum consectetur libero id. Lectus urna duis convallis convallis. Bibendum ut tristique et egestas quis ipsum suspendisse. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget. Placerat vestibulum lectus mauris ultrices eros in cursus. Sed turpis tincidunt id aliquet. Tristique senectus et netus et malesuada fames. Ut placerat orci nulla pellentesque dignissim enim sit amet venenatis. Vitae proin sagittis nisl rhoncus mattis. Diam donec adipiscing tristique risus nec. Venenatis tellus in metus vulputate eu scelerisque felis. Dis parturient montes nascetur ridiculus mus mauris vitae. Scelerisque purus semper eget duis at tellus. Vel elit scelerisque mauris pellentesque pulvinar pellentesque. Dictum sit amet justo donec. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Tristique et egestas quis ipsum suspendisse ultrices gravida dictum fusce. Facilisi cras fermentum odio eu feugiat pretium nibh ipsum. Congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque. Sed felis eget velit aliquet sagittis id. Fermentum leo vel orci porta. Lectus vestibulum mattis ullamcorper velit sed ullamcorper. Vitae proin sagittis nisl rhoncus. Habitant morbi tristique senectus et netus. Nisl vel pretium lectus quam id leo in vitae turpis."
 
 
 class EmbeddingUser(HttpUser):
@@ -161,6 +178,9 @@ def on_test_stop(environment, **kwargs):
         workspace_name=log_analytics_workspace_name,
     )
 
+    time_range = f"TimeGenerated > datetime({test_start_time.strftime('%Y-%m-%dT%H:%M:%SZ')}) and TimeGenerated < datetime({test_stop_time.strftime('%Y-%m-%dT%H:%M:%SZ')})"
+    logging.info(f"Query time range: {time_range}")
+
     metric_check_time = test_stop_time - timedelta(seconds=10)
     check_results_query = f"""
     ApiManagementGatewayLogs
@@ -168,8 +188,6 @@ def on_test_stop(environment, **kwargs):
     | count
     """
     query_processor.wait_for_non_zero_count(check_results_query)
-
-    time_range = f"TimeGenerated > datetime({test_start_time.strftime('%Y-%m-%dT%H:%M:%SZ')}) and TimeGenerated < datetime({test_stop_time.strftime('%Y-%m-%dT%H:%M:%SZ')})"
 
     query_processor.add_query(
         title="Overall request count",
